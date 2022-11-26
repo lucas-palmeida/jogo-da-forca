2792078caae3a7a7cbed682ff70f9094c6244f22
lucas-palmeida <lucaspacheco95@gmail.com>
2022-11-26 18:26:58 -0300

    feat: html/css finalizados, comecando componentizacao e funcionalidades

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
+  const letra = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="conteudo">
+      <div className="superior">
+        <div className="imagem">
+          <img src="assets/images/forca0.png" alt="forca" />
+        </div>
+        <div className="palavras">
+          <button className="escolher-palavra">Escolher Palavra</button>
+          <p className="palavra-sorteada">_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _</p>
+          {/* 16 */}
+        </div>
+      </div>
+      <div className="inferior">
+        <div className="teclado">
+          {letra.map((l) => <button className="letra">{l}</button>)}
+        </div>
+        <div className="chute">
+          <p>Já sei a palavra!</p>
+          <input type="text" className="campo-chute"></input>
+          <button>Chutar</button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
